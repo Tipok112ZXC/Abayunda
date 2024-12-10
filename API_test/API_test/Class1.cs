a6083e9e11fcc918541d3d82c02956e17cdcb350
Гость <@B302-12.ygk.ru>
2024-12-10 15:14:01 +0300

    first commit

---
+++ API_test/API_test/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace API_test
+{
+
+    public class User
+    {
+        public Data data { get; set; }
+        public Support support { get; set; }
+    }
+
+    public class Data
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int year { get; set; }
+        public string color { get; set; }
+        public string pantone_value { get; set; }
+    }
+
+    public class Support
+    {
+        public string url { get; set; }
+        public string text { get; set; }
+    }
+
+}
---
+++ API_test/API_test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_test
+{
+    class user
+    { 
+    
+    
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string URL = "https://reqres.in/api/users/2";
+            var request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.Proxy.Credentials = new NetworkCredential("student","student");
+            var response = request.GetResponse();
+            var respoonse = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            String Text = reader.ReadToEnd();
+            var JsonText = JsonConvert.DeserializeObject<User>(Text);
+            Console.WriteLine($"{JsonText.data.id}{JsonText.data.last_name}{JsonText.data.first_name}{JsonText.data.email}");
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
